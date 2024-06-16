c9a756799144e3a0bc12ddc5d48a87dd8ab41a1b
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-06-16 17:54:41 -0500

    refactor: limpia importaciones sin uso

--- src/modules/patients/api/psychologist/psychologist_router.py
+++ src/modules/patients/api/psychologist/psychologist_router.py
@@ -2,25 +2,24 @@ from typing import List
 
 from fastapi import APIRouter, Security
 from fastapi_injector import Injected
-from fastapi_jwt import JwtAuthorizationCredentials
 from mediatr import Mediator
 
 from src.common.api.authorization import admin_only
-from src.common.infrastructure.token.access_security import access_security
-from src.modules.patients.application.interactors.get_psychologists.get_psychologist_list_query import \
-    GetPsychologistListQuery
-from src.modules.patients.application.interactors.get_psychologists.get_psychologist_response import \
-    GetPsychologistListResponse
-
 from src.common.domain.value_objects.cedula import Cedula
+from src.common.infrastructure.token.access_security import access_security
 from src.modules.patients.api.psychologist.add_child_dto import AddChildDto
 from src.modules.patients.application.interactors.add_child.add_child_command import AddChildCommand
 from src.modules.patients.application.interactors.ejemplo_get_psychologist_name.get_psychologist_name_query import \
     GetPsychologistNameQuery
 from src.modules.patients.application.interactors.get_children.get_children_query import GetChildrenQuery
+from src.modules.patients.application.interactors.get_psychologists.get_psychologist_list_query import \
+    GetPsychologistListQuery
+from src.modules.patients.application.interactors.get_psychologists.get_psychologist_response import \
+    GetPsychologistListResponse
 from src.modules.patients.domain.child.scholar_grade import ScholarGrade
 
-router = APIRouter(prefix="/psychologists", tags=["psychologists"], dependencies=[Security(access_security), Security(admin_only)])
+router = APIRouter(prefix="/psychologists", tags=["psychologists"],
+                   dependencies=[Security(access_security), Security(admin_only)])
 
 
 @router.get("/", response_model=List[GetPsychologistListResponse])
@@ -59,4 +58,3 @@ async def get_children(psychologist_cedula: str, mediator: Mediator = Injected(M
         cedula=Cedula(psychologist_cedula)
     )
     return await mediator.send_async(query)
-
