b11222128f528bac2b4386f526e6dc0d93795fb1
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-06-16 20:14:17 -0500

    feat: agrega endpoint para obtener datos del usuario
    Co-Authored-By: Steven Quispe <78981969+stevenq9@users.noreply.github.com>

--- src/modules/users_management/api/users/user_router.py
+++ src/modules/users_management/api/users/user_router.py
@@ -10,15 +10,16 @@ from src.common.domain.value_objects.email import Email
 from src.common.infrastructure.token.access_security import access_security
 from src.modules.users_management.api.users.add_psychologist_user_dto import AddPsychologistUserDto
 from src.modules.users_management.api.users.update_psychologist_user_dto import UpdatePsychologistUserDto
+from src.modules.users_management.api.users.user_data_dto import UserDataDto
 from src.modules.users_management.application.interactors.add_psychologist_user.add_psychologist_user_command import \
     AddPsychologistUserCommand
 from src.modules.users_management.application.interactors.update_psychologist_user.update_psychologist_user_command import \
     UpdatePsychologistUserCommand
 
-router = APIRouter(prefix="/users", tags=["users"],  dependencies=[Security(access_security), Security(admin_only)])
+router = APIRouter(prefix="/users", tags=["users"],  dependencies=[Security(access_security)])
 
 
-@router.post("/psychologists", status_code=status.HTTP_201_CREATED)
+@router.post("/psychologists", status_code=status.HTTP_201_CREATED, dependencies=[Security(admin_only)])
 async def add_psychologist_user(
         add_psychologist_user_dto: AddPsychologistUserDto,
         mediator: Mediator = Injected(Mediator),
@@ -33,7 +34,7 @@ async def add_psychologist_user(
     await mediator.send_async(command)
 
 
-@router.put("/psychologists/{cedula}", status_code=status.HTTP_200_OK)
+@router.put("/psychologists/{cedula}", status_code=status.HTTP_200_OK, dependencies=[Security(admin_only)])
 async def update_psychologist_user(
         cedula: str,
         update_psychologist_user_dto: UpdatePsychologistUserDto,
@@ -50,4 +51,12 @@ async def update_psychologist_user(
     await mediator.send_async(command)
 
 
-
+@router.get("/me", response_model=UserDataDto)
+async def get_current_user_data(
+        credentials: JwtAuthorizationCredentials = Security(access_security)
+):
+    cedula = None if not credentials.subject.keys().__contains__("cedula") else str(credentials["cedula"])
+    return UserDataDto(
+        cedula=cedula,
+        role=str(credentials["role"]),
+    )
