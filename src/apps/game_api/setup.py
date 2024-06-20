f0c38a8cff91d383f23a3e1ad46aedf959f156ea
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-06-20 00:00:21 -0500

    build: configura cors en la api

--- src/apps/game_api/setup.py
+++ src/apps/game_api/setup.py
@@ -3,6 +3,7 @@ import os
 from fastapi import FastAPI
 from fastapi_injector import InjectorMiddleware, attach_injector
 from injector import Injector
+from starlette.middleware.cors import CORSMiddleware
 
 from src.apps.game_api.modules import modules
 from src.common.api.di import add_mediator, add_event_bus
@@ -22,5 +23,12 @@ def create_fast_api_app() -> FastAPI:
     add_database(injector)
     install_modules(injector, app, modules)
     app.add_middleware(InjectorMiddleware, injector=injector)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:8060"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     attach_injector(app, injector)
     return app
