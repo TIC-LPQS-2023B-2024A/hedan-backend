3ada3684dcae70e1a76e4fdbe12e2b4b500711a9
Anthony Simbaña <90488409+AnthonySimbana@users.noreply.github.com>
2024-06-23 18:11:37 -0500

    feat: Agrega consultas de tests reports

--- src/modules/results_analysis/api/routers.py
+++ src/modules/results_analysis/api/routers.py
@@ -2,6 +2,8 @@ from typing import List
 
 from fastapi import APIRouter
 
-routers: List[APIRouter] = [
+from src.modules.results_analysis.api.test_reports.test_reports_router import router as test_reports_router
 
+routers: List[APIRouter] = [
+    test_reports_router
 ]
--- src/modules/results_analysis/results_analysis_module.py
+++ src/modules/results_analysis/results_analysis_module.py
@@ -1,16 +1,32 @@
 from typing import cast
 
+from fastapi import FastAPI
+from fastapi_injector import request_scope
 from injector import Injector
+from mediatr import Mediator
 
-from src.common.application.event_bus import EventBus
-from src.common.event_bus_setup import register_event_bus_handlers
-from src.common.infrastructure.bus.memory.in_memory_event_bus import InMemoryEventBus
+from src.common.api.router_installer import RouterInstaller
+from src.common.mediator_setup import register_mediator_handlers
 from src.common.module import Module
-from src.modules.results_analysis.application.integration_events_handlers.integration_events_handlers import \
-    game_event_handlers
+from src.modules.results_analysis.api.routers import routers
+from src.modules.results_analysis.application.interactors.get_test_response.get_tests_reports_query_service import \
+    GetTestsReportsQueryService
+from src.modules.results_analysis.application.interactors.handlers import handlers
+from src.modules.results_analysis.infrastructure.persistence.sqlalchemy.query_services.sql_alchemy_get_tests_resports_query_service import \
+    SqlAlchemyGetTestsReportsQueryService
 
 
-class ResultsAnalysisModule(Module):
+class ResultsAnalysisModule(Module, RouterInstaller):
     @staticmethod
     def install(injector: Injector) -> None:
-        register_event_bus_handlers(cast(InMemoryEventBus, injector.get(EventBus)), game_event_handlers)
+        ResultsAnalysisModule.__register_services_implementation(injector)
+        register_mediator_handlers(injector.get(Mediator), handlers)
+
+    @staticmethod
+    def install_routers(fast_api_app: FastAPI):
+        [fast_api_app.include_router(router) for router in routers]
+
+    @staticmethod
+    def __register_services_implementation(injector: Injector):
+        injector.binder.bind(GetTestsReportsQueryService, to=SqlAlchemyGetTestsReportsQueryService,
+                             scope=request_scope)
